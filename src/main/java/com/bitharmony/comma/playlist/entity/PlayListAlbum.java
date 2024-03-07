24c95f70a63cd9b93a857550b846355b8dd86882
ppusda <ppusda1234@gmail.com>
2024-03-07 17:25:02 +0900

    feat: Playlist Entity 생성

---
+++ src/main/java/com/bitharmony/comma/playlist/entity/PlayListAlbum.java
@@ -0,0 +1,33 @@
+package com.bitharmony.comma.playlist.entity;
+
+import com.bitharmony.comma.album.album.entity.Album;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlayListAlbum {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "playlist_id")
+    private Playlist playlist;
+
+    @ManyToOne
+    @JoinColumn(name = "album_id")
+    private Album album;
+}
