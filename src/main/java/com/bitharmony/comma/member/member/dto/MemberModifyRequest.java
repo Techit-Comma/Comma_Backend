d5de726f757decbf5f9581d070a5ad28c9ea114f
ppusda <ppusda1234@gmail.com>
2024-03-23 18:52:54 +0900

    refactor: 수정 request 형식 수정

--- src/main/java/com/bitharmony/comma/member/member/dto/MemberModifyRequest.java
+++ src/main/java/com/bitharmony/comma/member/member/dto/MemberModifyRequest.java
@@ -2,11 +2,16 @@ package com.bitharmony.comma.member.member.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record MemberModifyRequest(
-        @NotBlank(message = "Nickname을 입력해주세요.") String nickname,
-        @NotBlank(message = "Email을 입력해주세요.") @Email(message = "올바른 email 형식을 입력해주세요.") String email
+        @NotBlank(message = "닉네임을 입력해주세요.")
+        @Size(message = "닉네임은 5글자 이상 100자 이하여야 합니다.", min = 5, max = 100)
+        String nickname,
+        @NotBlank(message = "이메일을 입력해주세요.")
+        @Email(message = "올바른 이메일 형식을 입력해주세요.")
+        String email
 ) {
 }
